87c5a77fbd309ce60341a1d28bc3104a111deae4
yaplans <yaplans@yandex.ru>
2019-04-10 23:46:48 +0700

    немного доработал server

--- sw/server/server.js
+++ sw/server/server.js
@@ -9,28 +9,50 @@ var path = require('path');
 const publicPath = path.join(__dirname + "/../public");
 app.use(express.static(publicPath));
 
+//~ массив игроков - хранится на сервере
+var players = [];
+
 
 
 //~ взял здесь
 //~ https://socket.io/get-started/chat/
 io.on('connection', function(socket){
+
 	// создадим пользователя, т.к. кто-то подключился к серверу
 	// с помощью socket
-	var user = Date.now();
-	console.log('a user connected id=' + `${user}`);
+	//~ var user = Date.now();
+	//~ console.log('a user connected id=' + `${user}`);
 	//~ console.log(socket);
 	// на установленном сокете создаем событие
 	//~ 	socket.emit('message', 'User ' + user + ' connected');
 
+//~ players.push(i++);
+//~ console.log(players);
+
 	//~ io.emit('message', 'User ' + user + ' connected');
 	//~ io.emit('user_id', user);
+
+
+
+
   
 	//~ Кто там залогинился?
+	// на полученный сокет (а их может быть много) навесим прослушку
 	socket.on('m_login', function(message){
-		console.log('socket.on m_login === ' + message);
-		//~ Разошлем клиентам
+		//~ запомним игрока
+		players.push({name: message, id: socket.id});
+		console.log(players);
+		// Разошлем всем - обновленный список игроков
+		io.emit('user_name', players);
+		
+		
+		
+		//~ console.log('socket.on m_login === ' + message);
+		//~ Разошлем клиентам - а нужно-ли?
 		//~ io.emit('user_name', message);
-		socket.broadcast.emit('user_name', message);
+		
+		//~ отдельное имя не нужно - нужен весь массив
+		//~ socket.broadcast.emit('user_name', message);
 	});
   
 	//~ socket.on('message.send', function(message){
@@ -42,16 +64,25 @@ io.on('connection', function(socket){
 		// всем кроме отправителя
 		//~ socket.broadcast.emit('socket.broadcast message', user + ": " + message);
 	//~ }); 
+	
+	
+	// на полученный сокет (а их может быть много) навесим прослушку
+	socket.on('disconnect', function(message){
+	//~ если, сокет отключился
 
-  
-  socket.on('disconnect', function(message){
-    console.log('user disconnected' + message);
-  });
+		//~ console.log("id = " + socket.id);
+		for(var i=0; i<players.length; i++){
+			//~ console.log(i + " = " + players[i].id);
+			if(players[i].id === socket.id){
+				// нашли игрока из этого сокета
+				players.splice(i, 1);// удаляем 1 элемент начиная с i-того
+			}
+		}
+		console.log('user disconnected ====' + message);
+		// Разошлем всем - обновленный список игроков
+		io.emit('user_name', players);
+	});
 });
-
-//~ io.on('message.send', function(message){
-	//~ console.log('io.on===message.send');
-//~ });
 
 
 

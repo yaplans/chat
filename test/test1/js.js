38c5814cc10b29ef7bd043f6001a567fca927b3c
yaplans <yaplans@mail.ru>
2019-05-04 00:36:08 +0600

    broadcast

--- test/test1/js.js
+++ test/test1/js.js
@@ -1,10 +1,55 @@
 var PORT = 4554;
+// Позже определить программно
+var BROADCAST_ADDR = '192.168.0.255'
+var MY_IP = "192.168.0.104"
+//
+var MY_ID = "" + Date.now()
+
+
 var dgram = require('dgram');
-//var client = dgram.createSocket('udp4');
+var client = dgram.createSocket('udp4');
+
+
+// готовим ответы
+client.on('error', (err) => {
+  console.log(`client error:\n${err.stack}`);
+  client.close();
+});
+
+client.on('message', (msg, rinfo) => {
+  console.log(`client got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+client.on('listening', () => {
+  const address = client.address();
+  console.log(`client listening ${address.address}:${address.port}`);
+});
+
+
+function l_send_request(){
+  var message = new Buffer(MY_ID)
+  client.send(message, 0, message.lenght, PORT, BROADCAST_ADDR, function(){
+    console.log("Send: '" + message + "'")
+  })
+}
+//подготовились ...
+
+// пуляем запросы
+client.bind(function() {
+  client.setBroadcast(true)
+  setInterval(l_send_request,500)
+})
+
+
+
+console.log('Ищем сервер')
+
+
+
 
 //Поищем сервер
-const message = Buffer.from("Hello")
-const client = dgram.createSocket("udp4")
+//const message = Buffer.from("Hello")
+//const client = dgram.createSocket("udp4")
 
 function l_listen() {
 client.bind(function(){
@@ -18,10 +63,26 @@ client.bind(function(){
 })
 }
 
+
+function l_server() {
+client.bind(function(){
+  client.setBroadcast(true)
+  client.send(message, 3000, "192.168.0.255", err => {
+    if(err) {
+      client.close()
+      throw err
+    }
+  })
+})
+}
+
+/*
 client.on('close',function(){
+// сервер не нашли "Я" - сервер
+
     console.log('Пока')
 })
-
+*/
 function l_log(){
   console.log('Привет')
 }
@@ -33,7 +94,16 @@ function l_close(){
 
 //setTimeout("alert('Привет')", 2000);
 //setTimeout(l_log, 2000);
-setTimeout(l_close, 14000);
+
+
+//===================//
+// Поищем сервер
+//===================//
+//l_listen()
+//===================//
+// Если сервер не нашли "Я" - сервер
+//===================//
+//setTimeout(l_close, 14000);
 
 
 /*

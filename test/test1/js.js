56370e824476898affbef83290feec423946bfe9
yaplans <yaplans@mail.ru>
2019-05-20 21:58:35 +0600

    Пн май 20 21:58:35 +06 2019

--- test/test1/js.js
+++ test/test1/js.js
@@ -1,9 +1,42 @@
 var PORT = 4554;
 // Позже определить программно
-var BROADCAST_ADDR = '192.168.0.255'
-var MY_IP = "192.168.0.104"
+//var BROADCAST_IP = '192.168.0.255'
+//var MY_IP = "192.168.0.104"
+var BROADCAST_IP = ''
+var MY_IP = ""
 
+/**
+ * Определяем свой и широковещательный 
+ * адреса
+ * */
 
+var ip = require('./ip.js')
+
+var timerId = setInterval(function(){
+  if(!isEmpty(ip)){
+    clearInterval(timerId)
+    console.log('Ok!')
+	BROADCAST_IP=ip.BROADCAST_IP
+	MY_IP = ip.MY_IP
+	console.log(BROADCAST_IP)
+	console.log(MY_IP)
+	
+	start_client()
+	start_server()
+  }
+},10)
+
+/**
+* Пуст ли объект
+*/
+function isEmpty(arg) {
+  for (var item in arg) {
+    return false;
+  }
+  return true;
+}
+
+// Получили...
 
 
 //
@@ -56,13 +89,13 @@ console.log('Ищем сервер')
 function l_send_request(){
 //  var message = new Buffer(MY_ID)
   var message = Buffer.from(MY_ID)
-  client.send(message, 0, message.length, PORT, BROADCAST_ADDR, function(){
+  client.send(message, 0, message.length, PORT, BROADCAST_IP, function(){
 //    console.log("Send: '" + message + "'")
     console.log("> '" + message + "'")
   })
 }
 
-start_client()
+//start_client()
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
@@ -87,7 +120,7 @@ server.bind(PORT);
 
 }
 
-start_server()
+//start_server()
 /*
 
 function l_listen() {

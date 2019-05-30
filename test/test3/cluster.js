8c4dc9ae2c944cee3d1afba078a27fd17fbf2191
yaplans <yaplans@mail.ru>
2019-05-30 18:04:33 +0600

    Чт мая 30 18:04:33 +06 2019

--- test/test3/cluster.js
+++ test/test3/cluster.js
@@ -5,13 +5,29 @@ var pid = process.pid;
 if (cluster.isMaster) {
 	var cpus = os.cpus().length;
 	console.log(`CPUs = ${cpus}`);   	
-	console.log('Master ' + `Pid = ${pid}`);   	
-	cluster.fork();
+	console.log('Master ' + `Pid = ${pid}`);
+	for(var i=0; i<cpus-1;i++){
+		const worker = cluster.fork();
+		worker.on('exit', function (){
+			console.log('=====================');
+			console.log('worker died ' + `Pid = ${worker.process.pid}`);
+			//const worker = 
+			cluster.fork();
+		});
+		// !!! Не работает
+		//worker.send('Hello from ' + `Pid = ${worker.process.pid}`);
+		worker.send('Hello from ' + worker.process.pid);
+	}
 }
 
 
 if (cluster.isWorker) {
-	var worker = require('./worker');
-	console.log('Worker ' + `Pid = ${pid}`);   	
+	//var worker = 
+	require('./worker');
+	console.log('Worker ' + `Pid = ${pid}`);
+//	worker.on('message', function (msg){
+	process.on('message', function (msg){
+		console.log('message ' + `: ${msg}`);
+	});
 }
 
--- test/test3/worker.js
+++ test/test3/worker.js
@@ -3,7 +3,7 @@ var pid = process.pid;
 // autocannon работает только если установить глобально
 
 http.createServer((req, res) => {
-	//for(var i =0; i<1e7;i++){	}
+	for(var i =0; i<1e7;i++){	}
  res.end('Hi!\n');
 }).listen(8080, () =>{
   console.log('Server Started! '+`Pid = ${pid}`);

54306edb3782207ba8aa7f751576e727cc5ccf4a
yaplans <yaplans@mail.ru>
2019-06-03 17:52:32 +0600

    Пн июн  3 17:52:30 +06 2019

--- test/test2/main.js
+++ test/test2/main.js
@@ -1,12 +1,21 @@
+/**
+ * Стартуем веб
+ * */
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+//  console.log(__dirname);
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
 
+
+/**
+ * Ищем сервер
+ * */
 
 var get_server_ip = require('./api_get_server_ip.js');
 

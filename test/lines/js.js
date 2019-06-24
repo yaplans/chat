df94303e2d71c9874890351cc711777d8beabc2e
yaplans <yaplans@mail.ru>
2019-06-24 18:21:36 +0600

    Пн июн 24 18:21:36 +06 2019

--- test/lines/js.js
+++ test/lines/js.js
@@ -12,6 +12,10 @@ var canvas = document.getElementById('myCanvas');
 var cell_width = canvas.width/9;
 var cell_height = canvas.height/9;
 
+var map_color = "#7F7F7F";
+// чтобы легче найти ошибку
+var mx = 10;
+var my = 10;
 var color = [
 "red",
 "green",
@@ -23,26 +27,20 @@ var color = [
 //~ Состояние
 //~ mark - пометили шар
 //~ wait - ждем отметки шара
-var state = "wait";
+//~ hod - нужно выкинуть шары
+var state = "hod";
+log_state(state,'log_state');
 
-
-
-//~ var ball = [];
+/**
+ * Инициализируем карту
+ * */
 var balls = [];
-
-//~ for(var j=0;j<9;j++){
-	//~ for(var i=0;i<9;i++){
-		//~ balls[i] = '';
-	//~ }
-	//~ balls[j] = ball;
-//~ }
-
 for(var i=0;i<9;i++){
 	balls[i] = ['','','','','','','','',''];
 }
-
-
 console.log(balls);
+
+
 
 window.onload = function(){
 	
@@ -59,6 +57,15 @@ window.onload = function(){
 	    m_log("displayCanvas start");	
 	    //~ displayCanvas();
 	    m_log("displayCanvas end!");	
+	    /**
+	     * Пошла игра
+	     * */
+	    if(state == "hod"){
+				m_hod();
+				state = "wait";
+				log_state(state,'log_state');
+			}
+	    
 		}
   , 1000);
 }
@@ -110,7 +117,7 @@ function displayMap(){
     var contextHTML = canvasHTML.getContext('2d');
     
     //~ закрасим прямоугольник
-    contextHTML.fillStyle = "#7F7F7F";
+    contextHTML.fillStyle = map_color;
     contextHTML.fillRect(0,0,canvasHTML.width,canvasHTML.height);
 
 
@@ -155,13 +162,34 @@ return;
 
 function getPosition(event)
 {
-  var x = event.x;
-  var y = event.y;
+	//~ console.log(event);
+	//~ console.log('clientX = '+event.clientX);
+	//~ console.log('clientY = '+event.clientY);
+	//~ console.log('layerX = '+event.layerX);
+	//~ console.log('layerY = '+event.layerY);
+	//~ console.log('pageX = '+event.pageX);
+	//~ console.log('pageY = '+event.pageY);
+	//~ console.log('screenX = '+event.screenX);
+	//~ console.log('screenY = '+event.screenY);
+	//~ console.log('x = '+event.x);
+	//~ console.log('y = '+event.y);
+	
 
+	//~ return;
+	
+  //~ var x = event.x;
+  //~ var y = event.y;
+  var x = event.pageX;
+  var y = event.pageY;
+console.log("x:" + x + " y:" + y);
   //~ var canvas = document.getElementById("myCanvas");
 
   x -= canvas.offsetLeft;
   y -= canvas.offsetTop;
+
+console.log("offsetLeft:" + canvas.offsetLeft + " offsetTop:" + canvas.offsetTop);
+
+console.log("x:" + x + " y:" + y);
 
 //~ округлим в большую сторону
 	var xx = Math.ceil(x/cell_width)-1;
@@ -169,16 +197,48 @@ function getPosition(event)
 	//~ alert("xx:" + xx + " yy:" + yy);
   //~ alert("x:" + x + " y:" + y);
   console.log("xx:" + xx + " yy:" + yy);
-  
-  //~ return {xx, yy};
 
 	var cc = color[m_rundom()];
-  //~ console.log(m_rundom());
   //~ console.log(cc);
+
+	    /**
+	     * Пошла игра
+	     * */
+	    if(state == "wait"){
+				console.log('state == "wait"');
+				if(balls[xx][yy]==''){
+					console.log('ничего не делаем');
+					// ничего не делаем
+				}else{
+					console.log('else balls');
+					state = "mark";
+					log_state(state,'log_state');
+					mx = xx;
+					my = yy;
+					log_state('mx:'+mx+'my:'+my,'mark_state');
+				}
+			}else{
+				console.log('else state == "wait"');
+				if(state == "mark"){
+					console.log('else state == "mark"');
+					if(balls[xx][yy]==''){
+						console.log('else state balls');
+						cc = balls[mx][my];
+						m_ball(cc, xx, yy);
+						log_state('cc:'+cc+' mx:'+mx+' my:'+my,'mark_state');
+						m_ball(map_color, mx, my);
+						state = "hod";
+						log_state(state,'log_state');
+						//~ console.log(balls);  
+					}
+					// ничего не делаем
+				}
+			}
+
   
-		m_ball(cc, xx, yy);
-		balls[xx][yy]=cc;
-console.log(balls);  
+		//~ m_ball(cc, xx, yy);
+		//~ balls[xx][yy]=cc;
+//~ console.log(balls);  
 } 
 
 function m_rundom() {
@@ -195,41 +255,97 @@ function getRandomArbitrary(min, max) {
 
 
 function m_hod() {
-	// округлим вниз
+/**
+		 * - создадим одномерный массив незаполненых ячеек
+		 * - рандом N
+*/
+		 var emptyCells = [];
+		 var n = 0;
+			for(var j=0;j<9;j++){
+				for(var i=0;i<9;i++){
+					if(balls[j][i] == ''){
+						emptyCells[n++] = {x:j, y:i};
+					}
+				}
+			}
+	console.log(emptyCells);
 
 	for(var i=0;i<3;i++){
 		var cc = color[m_rundom()];
-		//~ доработать
-		var z = Math.floor(getRandomArbitrary(0, 100) );
-		var n=0;
-		var m=0;
-		var nn=0;
-		var mm=0;
+		
+		/**
+		 * Теперь координаты
+		 * */
+		var z = Math.floor(getRandomArbitrary(0, n) );
 
-		for(var j=0;j<z;j++){
-			if(balls[n][m]==''){
-				//~ запомним индексы
-				nn=n;
-				mm=m;
-				//~ следующая итерация
-				m++;
-				if(m==9){
-					n++;
-					m=0;
-				}
-				if(n==9){
-					n=0;
-				}
-			}
-		}
-
-
-		m_ball(cc, xx, yy);
-		balls[xx][yy]=cc;
+		m_ball(cc, emptyCells[z].x, emptyCells[z].y);
+		balls[emptyCells[z].x][emptyCells[z].y]=cc;
+		//~ var removedItem = fruits.splice(pos, 1); // так можно удалить элемент
+		emptyCells.splice(z, 1); 
+		console.log(i);  
+		console.log(emptyCells);  
 	}
-	
+	state = 'wait';
 	
 }
 
 
 
+//~ function m_hod() {
+
+	//~ for(var i=0;i<3;i++){
+		//~ var cc = color[m_rundom()];
+		// доработать
+		//~ var z = Math.floor(getRandomArbitrary(0, 100) );
+		//~ var n=0;
+		//~ var m=0;
+		//~ var nn=0;
+		//~ var mm=0;
+
+		//~ for(var j=0;j<z;j++){
+			//~ if(balls[n][m]==''){
+				// запомним индексы
+				//~ nn=n;
+				//~ mm=m;
+				// следующая итерация
+				//~ m++;
+				//~ if(m==9){
+					//~ n++;
+					//~ m=0;
+				//~ }
+				//~ if(n==9){
+					//~ n=0;
+				//~ }
+			//~ }
+		//~ }
+
+
+		//~ m_ball(cc, nn, mm);
+		//~ balls[nn][mm]=cc;
+	//~ }
+	
+	
+//~ }
+
+
+
+function log_state(st,el){
+//~ switch(el) {
+  //~ case 'log_state':  // if (x === 'value1')
+    //~ ...
+    //~ [break]
+
+  //~ case 'mark_state':  // if (x === 'value2')
+    //~ ...
+    //~ [break]
+
+  //~ default:
+    //~ ...
+    //~ [break]
+//~ }
+	
+	//~ document.getElementById("log_state").innerHTML = st;
+	document.getElementById(el).innerHTML = st;
+	
+
+}
